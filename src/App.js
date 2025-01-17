75e416677d2685945129cdff4bcf1976beede5b4
Rashmiit <rashmiitrr@gmail.com>
2025-01-17 23:39:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Component/Navbar';
 import Home from './Component/Home';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <>
     <Navbar />
+    {/* <Footer /> */}
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
+    <Footer />
 
     
     </>
